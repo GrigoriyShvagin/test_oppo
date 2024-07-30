6dcd51a3ab33e881a5f5ea0e7026bc9fb4bcfa46
geteroseksuAll <Shvagin04@mail.ru>
2024-07-30 16:11:39 +0300

    test

--- src/Layouts/AppLayout.vue
+++ src/Layouts/AppLayout.vue
@@ -1,10 +1,18 @@
 <template>
-  <router-view />
-  <Footer />
+  <div class="content">
+    <router-view />
+    <Footer />
+  </div>
 </template>
 
 <script setup>
 import Footer from "../components/Footer.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -195,8 +195,8 @@ onMounted(() => {});
 .main {
   display: flex;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background: var(--main-bg);
   color: #fff;
   position: relative;
